043e1046f6f76a27bd455453f1eeae823a7efce9
gheryd <gheryd@gmail.com>
2024-05-13 08:15:16 +0200

    init code mirror

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { CodemirrorComponent } from '@ctrl/ngx-codemirror';
+import * as CodeMirror from 'codemirror';
+import { EditorConfiguration } from 'codemirror';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <ngx-codemirror
+    [(ngModel)]="content"
+    [options]="options"
+    (codeMirrorLoaded)="onLoaded($event)"
+  ></ngx-codemirror>`,
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  
   title = 'angular-code-mirror-study';
+  content = `const v = "prova";
+    function showV(){
+      alert(v);
+    }
+  `;
+
+  @ViewChild(CodemirrorComponent)
+  editor!: CodemirrorComponent
+  protected options: EditorConfiguration = {
+    lineNumbers: true,
+    theme: 'material',
+    mode: 'javascript',
+  }
+
+
+  constructor(){
+
+  }
+
+  ngAfterViewInit(): void {
+    this.editor.codeMirrorGlobal;
+    console.log("ngAfterViewInit", {editor: this.editor,codeMirrorGlobal: this.editor.codeMirrorGlobal });
+    
+    
+  }
+
+  protected onLoaded(data: CodemirrorComponent){
+    console.log("onLoaded", data.codeMirror);
+    (window as any).gerry = data;
+    console.log("CodeMirror", {CodeMirror});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+import { FormsModule } from '@angular/forms';
+// import * as CodeMirror from "codemirror";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CodemirrorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
