478587069a12269b3700dbd2669d02dc5f2a8544
gheryd <gheryd@gmail.com>
2024-05-19 16:50:54 +0200

    add autocomplete

--- src/app/editor1.component.ts
+++ src/app/editor1.component.ts
@@ -1,9 +1,10 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
 import { EditorStateConfig, EditorState, Extension, Text } from "@codemirror/state";
-import { javascript } from '@codemirror/lang-javascript';
-import { autocompletion } from '@codemirror/autocomplete';
+import { javascript, javascriptLanguage } from '@codemirror/lang-javascript';
+import { autocompletion, CompletionContext, CompletionResult } from '@codemirror/autocomplete';
 import {EditorView} from '@codemirror/view';
 import {basicSetup} from "codemirror";
+import {syntaxTree} from "@codemirror/language";
 
 @Component({
     selector:"editor1",
@@ -22,8 +23,11 @@ export class Editor1Component implements AfterViewInit {
    
  
     ngAfterViewInit(): void {
-        console.log("ngAfterViewInit");
-        
+
+        const jsDocCompletions = javascriptLanguage.data.of({
+            autocomplete: (context: CompletionContext) => this.suggest(context)
+        })
+
         const doc = Text.of([
             `console.log("Hello, CodeMirror 6!");`,
             `const s = "ciao";`,
@@ -35,7 +39,7 @@ export class Editor1Component implements AfterViewInit {
             extensions: [
                 basicSetup,
                 javascript(), 
-                autocompletion()
+                jsDocCompletions
             ]
         };
         const state:EditorState = EditorState.create(stateConfig);
@@ -53,5 +57,52 @@ export class Editor1Component implements AfterViewInit {
     protected onClickReplaceCurrentPosition(){
         let transaction = this.editor.state.update({changes: {from: this.editor.state.selection.main.head, insert: "//prova current position\n"}});
         this.editor.dispatch(transaction);
+    }
+
+    private suggest(context: CompletionContext): CompletionResult|null{
+        // const r = context.tokenBefore(["variable", "keyword"]);
+        const nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1)
+        // console.log("node before",  nodeBefore.name, {nodeBefore});
+        const textBefore = context.state.sliceDoc(nodeBefore.from, context.pos)
+        switch(nodeBefore.name){
+            case "VariableDeclaration":
+                return {
+                    from: context.pos,
+                    options: [
+                        {displayLabel: "provaVar",label: "provaVar", type:"Variable", },
+                        {displayLabel: "provinoVar", label: "provino", type:"Variable"},
+    
+                    ]
+                }
+            
+            case "VariableDefinition":
+                break;
+            case "VariableName":
+                break;
+            case "Equals":
+                return {
+                    from: context.pos,
+                    options: [
+                        {displayLabel: "prova",label: "\"prova\"", type:"String", },
+                        {displayLabel: "provino", label: "\"provino\"", type:"String"},
+    
+                    ]
+                }
+            case "String":
+                const tagBefore = /"\w*$/.exec(textBefore);
+                // console.log({tagBefore})
+                return {
+                    from: tagBefore ? nodeBefore.from + tagBefore.index : context.pos,
+                    options: [
+                        {displayLabel: "prova",label: "\"prova\"", type:"String", },
+                        {displayLabel: "provino", label: "\"provino\"", type:"String"},
+    
+                    ]
+                }
+            
+        }
+
+        return null;
+        
     }
 }
